715e9df4abf6d386742002c64a98a79c3df04667
gmacin18 <56467238+gmacin18@users.noreply.github.com>
2022-10-23 13:36:10 +0300

    i couldnt read the message but children are created

--- Assignment-1/Part3/driver.c
+++ Assignment-1/Part3/driver.c
@@ -10,6 +10,7 @@
 
 int main(int argc, char *argv[])
 {	
+	//taking inputs from command line
 	int N = atoi(argv[2]);
 	char stringmessage[50];
 	strcpy(stringmessage,argv[3]);
@@ -19,18 +20,21 @@ int main(int argc, char *argv[])
 	}
         
 	pid_t pid;
-	        
+	//pid and fork operations         
 	pid = fork();
         if (pid == 0) { 
+		//parent process
          	printf("Parent: Playing Chinese whisper with %d processes \n", N);
 	       	exit(0);
 	}
 	else if (pid > 0){
         wait(NULL);
-       	for(int i=1; i<=N; i++){
+       	for(int i=1; i<N; i++){
             pid = fork();
             if (pid == 0) {
-            } 
+		    //child operations should be her but i could not completed child part......
+            	break;
+	    } 
         }
         }
 		
